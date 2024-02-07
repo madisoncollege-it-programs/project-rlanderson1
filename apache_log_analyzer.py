71cda133fa59085c3842d2560b0558d68cc719f1
rlanderson1-matc <rlanderson1@madisoncollege.edu>
2024-02-07 17:50:36 -0600

    Submitting Milestone 1.

---
+++ apache_log_analyzer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" 
+apache_log_analyzer.py
+
+
+# Author:  Rae Denruiter
+# Email:  rlanderson1@wisc.edu
+# Description:  This script is part of the course project.  It automates the process of cloning a repository, updating README.md, 
+# and interesting with the user to continue or exit the 
+# program.
+"""
+
+# Step 2 - Add Output
+output_message = "This script automates the process of cloning a course repository, updating README.md, and interacting with the user."
+
+print(output_message)
+
+# Step 3 - Ask for Input
+user_input = input("Would you like to continue? (y/n): ") 
+print(f"User input received: {user_input}")
