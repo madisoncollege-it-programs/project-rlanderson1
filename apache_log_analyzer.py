b488619cc83ef294598fd7c3c82b2a3f2015ebfb
rlanderson1-matc <rlanderson1@madisoncollege.edu>
2024-02-22 13:27:12 -0600

    Resubmitting Milestone 4 for grading.

--- apache_log_analyzer.py
+++ apache_log_analyzer.py
@@ -15,15 +15,15 @@ output_message = "This script automates the process of cloning a course reposito
 print(output_message)
 
 # Step 3 - Ask for Input
-#user_input = input("Would you like to continue? (y/n): ") 
-#print(f"User input received: {user_input}")
+user_input = input("Would you like to continue? (y/n): ") 
+print(f"User input received: {user_input}")
 #Setting the apache log entry to the standard format with multiple log entries
 """apache_log = 111.222.333.123 HOME - [01/Feb/1998:01:08:39 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 198 "http://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Macintosh; I; PPC)" 
 111.222.333.124 HOME - [01/Feb/1998:01:08:46 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 28083 "http://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Macintosh; I; PPC)" 
 111.222.333.125 AWAY - [01/Feb/1998:01:08:53 -0800] "GET /bannerad/ad7.gif HTTP/1.0" 401 9332 "http://www.referrer.com/bannerad/ba_ad.htm" "Mozilla/4.01 (Macintosh; I; PPC)" 
 111.222.333.126 AWAY - [01/Feb/1998:01:09:14 -0800] "GET /bannerad/click.htm HTTP/1.0" 501 207 "http://www.referrer.com/bannerad/menu.htm" "Mozilla/4.01 (Macintosh; I; PPC)"""
 
-apache_log_file = open("m4-access.log", "r")
+apache_log_file = open("m5-access.log", "r")
 
 apache_log = apache_log_file.read()
 
@@ -44,6 +44,7 @@ for entry in apache_log_entries:
     summary = (f"{ip_address} - {return_code}")
     #print(f"IPAddress is: {ip_address}")
     print(summary)
-    apache_log_analysis.write(summary + "n\")
+    apache_log_analysis.write(summary + "\n")
 apache_log_analysis.close()
+
 
