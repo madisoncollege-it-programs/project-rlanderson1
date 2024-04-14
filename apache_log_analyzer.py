b6b57133395f712d8e34cd34c146f087eeb9547f
rlanderson1-matc <rlanderson1@madisoncollege.edu>
2024-04-14 18:02:03 -0500

    Submitting Milestone 9 for grading.

--- apache_log_analyzer.py
+++ apache_log_analyzer.py
@@ -1,89 +1,66 @@
 #!/usr/bin/env python3
-""" 
-apache_log_analyzer.py
 
 # Author:  Rae Denruiter
 # Email:  rlanderson1@wisc.edu
 # Description:  "Analyze an Apache web log.  We will look to see if there is one trying to hack our website." 
-"""
 
-import sys
-import subprocess
+import argparse
+import sys,subprocess
 
+#Takes in an apache log file and creates a summary of the top 5 ip address. Return the output
+def IPAddressCount(apache_log_file_name):  #Arguement
+    command = f"cat '{apache_log_file_name}' | cut -d ' ' -f1 | sort -n | uniq -c | sort -n | tail -n5"  #Create a formated string w/o being hard codeded. Pythons know to use apache_log_name as it's arguement
+    result = subprocess.run(command, stdout=subprocess.PIPE, shell=True)  # pass in the command , stndout is able to access data,  and then Pythhon is going to open a diff. shell to run this process w/o make it a list
+    return result.stdout.decode()   #Decode function. Return our results witht he stdout, we are grabbing the output of the above line, decode that and the pass that back
+
+#Function that takes in an individual log entry and parses
 def ParseLogEntry(logentry):
 
     #Split the string to get the return code
-    apache_log_entry_items = logentry.split(' ') 
+    apache_log_entry_items = logentry.split(' ')
 
     #Grabbing the correct elements of the apache log entry to output IP and return code
     ip_address = apache_log_entry_items[0]
     return_code = apache_log_entry_items[8]
     return [ip_address, return_code]
 
-def IPAddressCount(apache_log_analyzer):
-    #Construct the command string
-    command = f"cat {apache_log_analyzer} | cut -d ' ' -f1 | sort | uniq -c | sort -n | tail -n5"
-
-    #Run the command using subprocess
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
-
-    #Return the resulting string
-    return result.stdout
-
-
 def main():
-
+    #Create argument parser     
     #Create a description to show to the user
-    description = "Analyze an Apache web log.  We will look to see if there is anyone trying to hack our website"
-
-    print(description)
+    parser = argparse.ArgumentParser(description="Analyze an Apache web log.")
+    parser.add_argument('-f', '--filename', type=str, required=True, help="Path to the Apache log file")
+    args = parser.parse_args()
+   
+       #print(description)
 
     #Arguement statement
-    if len(sys.argv) > 1:
-    #Argument list
-            user_response = sys.argv[1]
+    #if len(sys.argv) > 1:
+        #Argument list
+        #user_response = sys.argv[1]
+    #Ask the user if they want to continue
+   # else:
+        #user_response = input("Would you like to continue?  (y or n)")
 
-        #Ask the user if they want to continue
+   # acceptable_responses = ['y','yes', 'yeah']
+
+   # if user_response.lower() in acceptable_responses:
+        
+    #Calling a function to get the summary information of our apache log file
+    results = IPAddressCount(args.filename)
+
+        #Open a new file to output our analysis
+    with open('apache_analysis.txt', 'w') as apache_log_analysis:
+        apache_log_analysis.write(results)
+        print(results)
+
+        #Closing our analysis file
+       # apache_log_analysis.close()
     
-    #If there isn't an argument
-    else:
-        user_response = input("Would you like to continue?  (y or n)")
-
-    acceptable_responses = ['y','yes', 'yeah']
-
-    if user_response.lower() in acceptable_responses:
-
-    #Make a one by one instead 
-        if user_response.lower():
-            # == 'y' or user_response.lower() == 'yes' or user_response.lower() == 'yeah':
-
-            #Setting the apache log file name
-            apache_log_analyzer = "m5-access.log"
-
-            #Call the  IPAddressCount function
-            result = IPAddressCount(apache_log_analyzer)
-
-            #Print the results to the screen
-            print(result)
-
-            #Write the results to the apache_analysisd.txt file
-            with open("apache_analysis.txt", "w") as file:
-                file.write(result)
-            
-        else:
-            print("You chose not to continue.  Exiting the program...")
-
-    else:
-        print("Invalid response.  Exiting the program.")
+    #else:
+       # print("You chose not to continue.  Exiting the program...")
 
 if __name__ == "__main__":
     main()
 
 
    
-
-
-
-   
-
-
